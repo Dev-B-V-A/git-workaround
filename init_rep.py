f57e66637c53f8d9b454dff3938a6ed6cf125267
Vladislav Bobylev <vladislav.bobylev@rfdyn.ru>
2017-10-20 15:08:30 +0300

    NEW: init: create mirror and clone from mir test rep, add testing branch to md

--- init_rep.py
+++ init_rep.py
@@ -1,4 +1,4 @@
-#! usr/bin/env python
+#! /usr/bin/env python
 import os
 from subprocess import check_output 
 from subprocess import call 
@@ -7,8 +7,13 @@ import argparse
 def call_cmd (cmd):
     call (cmd)
 
-def get_clone_dst_path ():
-    return './dest'
+def get_clone_rep_name (src_str):
+    return src_str[src_str.rfind('/') + 1: src_str.find('.git')]
+
+def call_clone (args):
+    cmd = ['git', 'clone']
+    cmd += args
+    call_cmd (cmd)
 
 def get_clone_addr ():
   parser = argparse.ArgumentParser (description="Clone git-repository")
@@ -17,11 +22,50 @@ def get_clone_addr ():
   return result_parse_args.src_clone
 
 def init_workspace_dir ():
-    clone_cmd = [ 'git', 'clone', get_clone_addr ()]
-    call_cmd (clone_cmd)
+#    address = get_clone_addr ()
+    address = 'https://github.com/Dev-B-V-A/Python.git'
+    clone_args = [ address ]
+    call_clone (clone_args)
+    return address
+
+def get_mirror_dir_name (git_rep_path):
+    return os.path.basename(git_rep_path) + '_MIR'
+
+def create_mirror_rep (git_rep_path):
+    clone_mir_args = [ '--mirror', git_rep_path, get_mirror_dir_name (git_rep_path)]
+    call_clone (clone_mir_args)
+    
+def create_clone_by_mirror (git_mirror_path, clone_name):
+    clone_args = [ git_mirror_path, clone_name ]
+    call_clone (clone_args)
+
+def create_md (git_mirror_path):
+    clone_md_args = [ git_mirror_path, './md']
+    call_clone (clone_md_args)
+
+def create_test_md (mir_path):
+    clone_test_args = [ mir_path, './test_md']
+    call_clone (clone_test_args)
+
+def add_remote_branches (test_md_path):
+    remote_cmd = ['git', 'remote', 'add', 'test_md', test_md_path]
+    call_cmd (remote_cmd)
+
+def configure_md ():
+    md_path = os.getcwd () + '/md'
+    os.chdir (md_path)
+    test_md_path = md_path + '/../test_md'
+    add_remote_branches (test_md_path)
 
 def main ():
-    init_workspace_dir ()
+    rep_address = init_workspace_dir ()
+    rep_name = get_clone_rep_name (rep_address)
+    rep_path = os.getcwd () + '/' + rep_name
+    mir_path = os.getcwd () + '/' + get_mirror_dir_name (rep_path)
+    create_mirror_rep (rep_path)
+    create_md (mir_path)
+    create_test_md (mir_path)
+    configure_md ()
 
 if __name__ == '__main__':
     main ()
